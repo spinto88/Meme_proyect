d8a2793456c6b305135cfcf657994de534db5f4c
Seba Facultad <spinto@df.uba.ar>
2023-12-12 16:45:36 -0300

    Some changes in run on other computers

--- Extraccion_datos_leskovec/Corte_y_rolling.py
+++ Extraccion_datos_leskovec/Corte_y_rolling.py
@@ -11,30 +11,35 @@ except:
 
 for filename in filenames: 
 
-    fileid = int(filename.replace('.csv', '').split('id')[1])
-    
-    data = pd.read_csv(filename)
+    try:
 
-    # Pongo el tiempo como el índice de los datos
-    data.time = pd.to_datetime(data.time)
-    data.set_index('time', inplace = True)
+        fileid = int(filename.replace('.csv', '').split('id')[1])
     
-    blog = data[data.media == 'B'].resample('1H').sum()
-    media = data[data.media == 'M'].resample('1H').sum()
+        data = pd.read_csv(filename)
 
-    new_index = blog.index.union(media.index)
+        # Pongo el tiempo como el índice de los datos
+        data.time = pd.to_datetime(data.time, format = "%Y-%m-%d %H:%M:%S" )
+        data.set_index('time', inplace = True)
 
-    media = media.reindex(new_index).fillna(0.00)
-    blog = blog.reindex(new_index).fillna(0.00)
+        blog = data[data.media == 'B'].resample('1H').sum()['frequency']
+        media = data[data.media == 'M'].resample('1H').sum()['frequency']
+
+        new_index = blog.index.union(media.index)
+
+        media = media.reindex(new_index).fillna(0.00)
+        blog = blog.reindex(new_index).fillna(0.00)
+
+        all_data = pd.DataFrame(index = new_index)
+        all_data['media'] = media.to_list()
+        all_data['blog'] = blog.to_list()
+ 
+        # We define a cut between 3 days before the global peak and 21 days after that
+        resample_dropped = all_data[(all_data.index > (all_data.sum(axis = 1).idxmax() - pd.to_timedelta('3D'))) & (all_data.index < (all_data.sum(axis = 1).idxmax() + pd.to_timedelta('21D')))]
+
+        # Rolling window of 24H to extract the trend
+        rolling_dropped = resample_dropped.rolling('1D', center=True).mean()
     
-    all_data = media.copy()
-    all_data.rename(columns = {'frequency': 'media'}, inplace=True)
-    all_data['blog'] = blog.frequency
-   
-    # We define a cut between 3 days before the global peak and 21 days after that
-    resample_dropped = all_data[(all_data.index > (all_data.sum(axis = 1).idxmax() - pd.to_timedelta('3D'))) & (all_data.index < (all_data.sum(axis = 1).idxmax() + pd.to_timedelta('21D')))]
+        rolling_dropped.to_csv('Series_a_ajustar/Corte_id{}.csv'.format(fileid))
 
-    # Rolling window of 24H to extract the trend
-    rolling_dropped = resample_dropped.rolling('1D', center=True).mean()
-    
-    rolling_dropped.to_csv('Series_a_ajustar/Corte_id{}.csv'.format(fileid))
+    except:
+        pass
